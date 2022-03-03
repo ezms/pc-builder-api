76a971ef6e1ade35658f6bf24fde67daff490a96
Vinícius de Freitas <viniciusfreitas.1.4.4@gmail.com>
2022-03-03 09:18:39 -0300

    feat: create fake routes in order routes and order blueprint

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, Flask
 
+from app.routes.order_routes import bp as bp_order
 
 blueprint = Blueprint("api", __name__, url_prefix="/")
 
 def init_app(app: Flask):
     app.config["JSON_SORT_KEYS"] = False
 
+    
+    blueprint.register_blueprint(bp_order)
     app.register_blueprint(blueprint)
---
+++ app/routes/order_routes.py
@@ -0,0 +1,20 @@
+from flask import Blueprint
+
+bp = Blueprint("blueprint_order", __name__, url_prefix="/order")
+
+def create_order():
+    ...
+
+def get_orders():
+    ...
+
+def update_orders():
+    ...
+
+def delete_orders():
+    ...
+
+bp.post("")(create_order)
+bp.get("")(get_orders)
+bp.patch("")(update_orders)
+bp.delete("")(delete_orders)
