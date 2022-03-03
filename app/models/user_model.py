5fd543561e7fe7eff01ad1acd3e8c395884b0f7c
Naftaly Junior <juniorinterlyte95@hotmail.com>
2022-03-03 07:45:34 -0300

    feat: added user_model

---
+++ app/models/user_model.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from sqlalchemy import Column, Integer, String
+from app.core.database import db
+
+
+@dataclass
+class User(db.Model):
+    user_id: int
+    name: str
+    email: str
+    password: str
+    cpf: str
+
+    __tablename__ = "users"
+
+    user_id = Column(Integer, primary_key=True)
+    name = Column(String(), nullable=False)
+    email = Column(String(), unique=True, nullable=False)
+    password = Column(String(100), nullable=False)
+    cpf = Column(String(11), nullable=False, unique=True)
