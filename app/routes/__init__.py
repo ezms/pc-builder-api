36b8de74e9ba62d09d90e3abbab869de73ef018b
Rafael Schug <rafaelschugbc@gmail.com>
2022-03-03 16:54:53 -0300

    feat: Category routes

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,11 +2,10 @@ from flask import Blueprint, Flask
 
 from app.routes.address_routes import bp_address
 from app.routes.cart_routes import bp as bp_cart
-
-from app.routes.user_routes import bp as bp_user
+from app.routes.category_routes import bp as bp_category
 from app.routes.order_routes import bp as bp_order
 from app.routes.product_routes import bp as bp_product
-
+from app.routes.user_routes import bp as bp_user
 
 blueprint = Blueprint("api", __name__, url_prefix="/")
 
@@ -19,5 +18,6 @@ def init_app(app: Flask):
     blueprint.register_blueprint(bp_cart)
     blueprint.register_blueprint(bp_user)
     blueprint.register_blueprint(bp_product)
+    blueprint.register_blueprint(bp_category)
 
     app.register_blueprint(blueprint)
