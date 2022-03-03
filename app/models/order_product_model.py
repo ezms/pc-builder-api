0a20398163dd44ea45809ff1eb739324490da052
Vinícius de Freitas <viniciusfreitas.1.4.4@gmail.com>
2022-03-03 09:28:01 -0300

    feat: order_product model created

---
+++ app/models/order_product_model.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, ForeignKey, Integer
+from dataclasses import asdict, dataclass
+
+from app.core.database import db
+
+@dataclass
+class OrdersModel(db.Model):
+    __tablename__ = "orders_products"
+
+    order_product_id: int = Column(Integer, primary_key=True)
+
+    order_id: int = Column(Integer, ForeignKey("orders.order_id"), nullable=False)
+    product_id: int = Column(Integer, ForeignKey("products.product_id"), nullable=False)
+
+    def asdict(self):
+        return asdict(self)
