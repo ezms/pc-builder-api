17ff1ce8e2182264307cc47edfe39a6394f01bb1
Naftaly Junior <juniorinterlyte95@hotmail.com>
2022-03-03 12:36:46 -0300

    feat: added password encryption to user model

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from sqlalchemy.orm import relationship, backref
 
 from sqlalchemy import Column, Integer, String
@@ -23,3 +25,14 @@ class UserModel(db.Model):
     )
 
     orders: list = relationship("OrdersModel", backref=backref("user", uselist=False))
+
+    @property
+    def password(self):
+        raise AttributeError("Password cannot be accessed!")
+
+    @password.setter
+    def password(self, password_to_hash):
+        self.password_hash = generate_password_hash(password_to_hash)
+
+    def verify_password(self, password_to_compare):
+        return check_password_hash(self.password_hash, password_to_compare)
