a0102e69ccfb7c6827b8c43c9fe0b7866ff1e04d
Naftaly Junior <juniorinterlyte95@hotmail.com>
2022-03-04 13:28:20 -0300

    feat: created get_user controller with protection

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -6,8 +6,8 @@ from app.controllers import user_controller
 bp = Blueprint("blueprint_user", __name__, url_prefix="/user")
 
 
-def get_user():
-    ...
+# def get_user():
+#     ...
 
 
 def update_user():
@@ -23,7 +23,7 @@ def register():
 
 
 bp.post("/register")(user_controller.create_user)
-bp.get("")(get_user)
+bp.get("")(user_controller.get_user)
 bp.patch("")(update_user)
 bp.delete("")(delete_user)
 bp.post("/login")(user_controller.login)
