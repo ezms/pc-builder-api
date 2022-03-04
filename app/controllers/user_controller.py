d4d3d864beae26b72bbef70a2e476f3c3b5f1587
Vinícius de Freitas <viniciusfreitas.1.4.4@gmail.com>
2022-03-03 21:44:33 -0300

    feat: create user route errors treated

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -1,6 +1,8 @@
 from http import HTTPStatus
 
 from flask import jsonify, request
+from psycopg2.errors import UniqueViolation
+from sqlalchemy.exc import DataError, IntegrityError
 
 from app.core.database import db
 from app.models.user_model import UserModel
@@ -9,14 +11,32 @@ from app.models.user_model import UserModel
 def create_user():
     data = request.get_json()
 
-    user = UserModel(
-        name=data["name"],
-        email=data["email"],
-        password=data["password"],
-        cpf=data["password"],
-    )
+    try:
+        user = UserModel(
+            name=data["name"].lower().title(),
+            email=data["email"].lower(),
+            password=data["password"],
+            cpf=data["cpf"],
+        )
 
-    db.session.add(user)
-    db.session.commit()
+        db.session.add(user)
+        db.session.commit()
+    except IntegrityError as e:
+        if isinstance(e.orig, UniqueViolation):
+            return {"error": "'email' or 'cpf' already exists!"}, HTTPStatus.CONFLICT
+    except DataError:
+        return {
+            "error": "'cpf' field must contain only 11 characters!"
+        }, HTTPStatus.BAD_REQUEST
+    except KeyError:
+        missing_fields = [
+            field
+            for field in ["name", "email", "password", "cpf"]
+            if field not in data.keys()
+        ]
+        return {
+            "available_fields": ["name", "email", "password", "cpf"],
+            "missing_fields": missing_fields,
+        }, HTTPStatus.UNPROCESSABLE_ENTITY
 
     return jsonify(user), HTTPStatus.CREATED
