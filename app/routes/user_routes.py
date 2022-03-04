a39cef722ca37712be5c1209eb57ec12118e6f90
Igor <igormgg@hotmail.com>
2022-03-04 12:59:54 -0300

    feat: cart creation on register almost done

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 
-from app.controllers import user_controller
+from app.controllers.user_controller import create_user
+
 
 bp = Blueprint("blueprint_user", __name__, url_prefix="/user")
 
@@ -25,7 +26,7 @@ def register():
     ...
 
 
-bp.post("/register")(user_controller.create_user)
+bp.post("/register")(create_user)
 bp.get("")(get_user)
 bp.patch("")(update_user)
 bp.delete("")(delete_user)
