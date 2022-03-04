59fb043be5a96d0c77b3f18faf621b2cfed80d93
Rafael Schug <rafaelschugbc@gmail.com>
2022-03-04 19:04:09 -0300

    feat: add product to cart by specifying product_id

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -29,6 +29,8 @@ class UserModel(db.Model):
 
     orders: list = relationship("OrdersModel", backref=backref("user", uselist=False))
 
+    cart = relationship("CartsModel", uselist=False)
+
     @property
     def password(self):
         raise AttributeError("Password cannot be accessed!")
