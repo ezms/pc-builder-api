bdb91baa6d0e464d7580c6151838632a772969cc
Vinícius de Freitas <viniciusfreitas.1.4.4@gmail.com>
2022-03-05 18:25:11 -0300

    feat: get cart function addded and get cart route implemented

--- app/routes/cart_routes.py
+++ app/routes/cart_routes.py
@@ -1,11 +1,9 @@
 from flask import Blueprint
-from app.controllers.cart_controller import add_product_to_cart, delete_cart
+
+from app.controllers.cart_controller import (add_product_to_cart, delete_cart,
+                                             get_cart)
 
 bp = Blueprint("blueprint_cart", __name__, url_prefix="/cart")
-
-
-def get_cart():
-    ...
 
 
 bp.post("/<int:product_id>")(add_product_to_cart)
